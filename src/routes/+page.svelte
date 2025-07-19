66ce1941264f322d4ea6f785a2e6525c8c1e3183
Toad169 <mamangpunten893@gmail.com>
2025-07-19 09:55:42 +0700

    yo

---
+++ src/routes/+page.svelte
@@ -0,0 +1,57 @@
+<section class="bg-white lg:grid lg:h-screen lg:place-content-center">
+  <div
+    class="mx-auto w-screen max-w-screen-xl px-4 py-16 sm:px-6 sm:py-24 md:grid md:grid-cols-2 md:items-center md:gap-4 lg:px-8 lg:py-32"
+  >
+    <div class="max-w-prose text-left">
+      <h1 class="text-4xl font-bold text-gray-900 sm:text-5xl">
+        Hai, perkenalkan nama saya
+        <strong class="text-indigo-600"> Shandy </strong>
+      </h1>
+
+      <p class="mt-4 text-base text-pretty text-gray-700 sm:text-lg/relaxed">
+        Dan saya adalah siswa Kelas 12 PPLG dari SMK Assa'idiyah Kudus
+      </p>
+
+      <!-- <div class="mt-4 flex gap-4 sm:mt-6">
+        <a
+          class="inline-block rounded border border-indigo-600 bg-indigo-600 px-5 py-3 font-medium text-white shadow-sm transition-colors hover:bg-indigo-700"
+          href="#"
+        >
+          Get Started
+        </a>
+
+        <a
+          class="inline-block rounded border border-gray-200 px-5 py-3 font-medium text-gray-700 shadow-sm transition-colors hover:bg-gray-50 hover:text-gray-900"
+          href="#"
+        >
+          Learn More
+        </a>
+      </div> -->
+    </div>
+
+    <img src="/img/ok1.jpg" alt="" class="mx-auto hidden max-w-md text-gray-900 md:block">
+    
+  </div>
+</section>
+
+<section>
+  <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
+    <div class="space-y-4 md:space-y-8">
+      <div class="max-w-xl">
+        <h2 class="text-2xl font-semibold text-gray-900 sm:text-3xl">
+          SMK Assa'idiyah Kudus
+        </h2>
+
+        <p class="mt-4 text-gray-700">
+          SMK Assa'idiyah Kudus adalah sekolah menengah kejuruan yang berlokasi Jl. Mbah Hamzah, Krapyak, Kirig, Mejobo, Kabupaten Kudus, Jawa Tengah 59381
+        </p>
+      </div>
+
+      <img
+        src="/img/i-hate-jews.jpg"
+        class="rounded"
+        alt=""
+      />
+    </div>
+  </div>
+</section>
